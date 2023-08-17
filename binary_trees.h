2eec21575f8bc69196c43419b4b91ff909e3ef11
Brook25 <bekelebrook24@gmail.com>
2023-08-17 10:10:59 +0000

    deletes a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -32,15 +32,11 @@ typedef struct binary_tree_s heap_t;
 
 /* binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);
-//static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
-//static size_t _height(const binary_tree_t *tree);
-//void binary_tree_print(const binary_tree_t *tree);
 
 /* tasks */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-//binary_tree_t *find_leaf(binary_tree_t *node);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
