a00a400fa0cb4b35521e18263a7001053665145d
Brook25 <bekelebrook24@gmail.com>
2023-08-17 09:51:35 +0000

    deletes a binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,14 +1,43 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - deletes abinary tree
- * @tree: tree to be deleted
+ * find_leaf - finds leaf of a sub tree
+ * @node: the root node of the sub tree
+ * Return: a leaf node or a node with no left or right nodes
  */
-void binary_tree_delete(binary_tree_t *tree)
+
+binary_tree_t *find_leaf(binary_tree_t *node)
 {
+	while (node->left || node->right) {
+                if (node->left)
+                        node = node->left;
+                else if (node->right)
+                        node = node->right;
+	}
+	
+	return node;
+}
+
+/**
+ * binary_tree_delete - deletes a binary tree and frees memory
+ * @tree: the tree to be deleted
+ * Return: nothing
+ */
+
+void binary_tree_delete(binary_tree_t *tree) 
+{
+	binary_tree_t *sub_t = tree;
+	binary_tree_t *parent;
 	if (!tree)
 		return;
-	binary_tree_delete(tree->right);
-	binary_tree_delete(tree->left);
-	free(tree);
+	while (sub_t) {
+	sub_t = find_leaf(sub_t);
+	parent = sub_t->parent;
+	if (parent && parent->left == sub_t)
+		parent->left = NULL;
+	else if (parent && parent->right == sub_t)
+		parent->right = NULL;
+	free(sub_t);
+	sub_t = parent;
+	}
 }
