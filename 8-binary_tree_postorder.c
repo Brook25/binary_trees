aeddde47c2f754bd82c4efe26e7997bca94a7892
Brook25 <bekelebrook24@gmail.com>
2023-08-20 09:47:09 +0000

    post order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,60 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
- * @tree: tree to traverse
- * @func: pointer to a function to call for each node
+ * traverse_and_print - traverses a tree in post-order traversal
+ * and prints the n value of the node
+ * @node: the node of the tree where traversal starts
+ * @func: function pointer to print n
+ * Return: node
+ */
+binary_tree_t *traverse_and_print(binary_tree_t *node)
+{
+	while (node->left || node->right) {
+		if (node->left)
+			node = node->left;
+		else if (node->right) {
+			node = node->right;
+		}
+	}
+	return (node);
+}
+
+/**
+ * binary_tree_inorder - does post-order traversal and prints n value
+ * of node by calling helper functions
+ * @tree: tree to be traversed
+ * @func: func used to print values
+ * Return: None
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
+	binary_tree_t *sub_tree;
+	if (!tree)
 		return;
 
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+	if (!func)
+		return;
+
+	sub_tree = (binary_tree_t *) tree;
+	sub_tree = traverse_and_print(sub_tree);
+
+	while (sub_tree != tree)
+        	{
+			func(sub_tree->n);
+	     		
+			if (sub_tree == sub_tree->parent->right)
+             		{
+                 		sub_tree = sub_tree->parent;
+                        	continue;
+             		}
+              		if (sub_tree == sub_tree->parent->left && !(sub_tree->parent->right))
+              		{
+				sub_tree = sub_tree->parent;
+                        	continue;
+                	}
+
+			sub_tree = traverse_and_print(sub_tree->parent->right);
+		}
+
+	func(sub_tree->n);
 }
