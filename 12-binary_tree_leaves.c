62692ec270c48a91859691408d50e63076f3d775
Brook25 <bekelebrook24@gmail.com>
2023-08-21 13:36:05 +0000

    calculates no of leaves of a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,19 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts the leaves of a binary tree
- * @tree: tree to count the leaves from
- *
- * Return: number of leaves
- *         0 if tree is NULL
+ * binary_tree_leaves - counts the number of leaves in a tree
+ * @tree: tree with no of leaves required
+ * Return: no of leaves
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t left_leaves;
+	size_t right_leaves;
 	if (!tree)
 		return (0);
-
-	if (!tree->left && !tree->right)
+	if (!(tree->left) && !(tree->right))
 		return (1);
 
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
 }
