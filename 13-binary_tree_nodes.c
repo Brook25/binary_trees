1fd5084b32f347160fc04e9e4690b36e008731a8
Brook25 <bekelebrook24@gmail.com>
2023-08-21 14:17:00 +0000

    calculates no of nodes with at least on child node

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_nodes - counts number of nodes with atleast
+ * child node
+ * @tree: tree with nodes to be counted
+ * Return: noumber of nodes
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree)
