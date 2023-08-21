f067f4150447b75d73ed64fcab6368d5f01057cd
Brook25 <bekelebrook24@gmail.com>
2023-08-21 12:34:45 +0000

    calculates size of a binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,16 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - measures the size of a binary tree
- * @tree: tree to measure the size of
- *
+ * binary_tree_size - calculates no of nodes of a binary tree
+ * @tree: tree to be traversed
  * Return: size of the tree
- *         0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	binary_tree_t *sub_tree;
+	size_t left_size;
 	if (!tree)
 		return (0);
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	sub_tree = (binary_tree_t *) tree;
+	
+	left_size = 1 + binary_tree_size(sub_tree->left);
+	return left_size + binary_tree_size(sub_tree->right);
 }
