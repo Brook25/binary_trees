985695da9946a3c4a94ce8a2e8acd21b66504268
Brook25 <bekelebrook24@gmail.com>
2023-08-22 14:36:36 +0000

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,21 +13,17 @@ int get_height(binary_tree_t *tree, int level)
 
 	if (!tree)
 		return (0);
-	//if (!(tree->right) && !(tree->left))
-	//	return (0);
-
-	//if (tree->left || tree->right)
-	//leve;
 	left_height = get_height(tree->left, level + 1);
 	
 	right_height = 1 + level + get_height(tree->right, level);
 
-	//printf("left->%d\n", left_height);
-	//printf("right->%d\n", right_height);
 	if (left_height >= right_height)
 		return left_height;
 	return right_height;
 }
+
+
+int get_height(binary_tree_t *tree, int level);
 
 /**
  * binary_tree_balance - returns the balance factor of a tree
@@ -39,8 +35,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	
-	//printf("%d\n", get_height(tree->left, 0));
-	//printf("%d\n", get_height(tree->right, 0));
-	//return 0;
 	return (get_height(tree->left, 0) - get_height(tree->right, 0));
 }
